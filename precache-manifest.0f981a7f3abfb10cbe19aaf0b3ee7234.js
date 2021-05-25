a943bf81bc2eb0b144b966aea9b864d76ea1402f
V3dant <vedantsingh5495@gmail.com>
2021-05-25 23:27:12 +0530

    Updates

---
+++ precache-manifest.0f981a7f3abfb10cbe19aaf0b3ee7234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ed95d77ace242ed48c9533921906af",
+    "url": "/oneminutequiz/index.html"
+  },
+  {
+    "revision": "56139d573214afc48d26",
+    "url": "/oneminutequiz/static/css/main.68c36139.chunk.css"
+  },
+  {
+    "revision": "664dcc2b6211eda5d8e6",
+    "url": "/oneminutequiz/static/js/2.c08403ab.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/oneminutequiz/static/js/2.c08403ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56139d573214afc48d26",
+    "url": "/oneminutequiz/static/js/main.ac311d91.chunk.js"
+  },
+  {
+    "revision": "3c377b249ecd7971d4f9",
+    "url": "/oneminutequiz/static/js/runtime-main.44fdd085.js"
+  }
+]);
